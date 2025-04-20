ac981660740586bbb7761cdef1d54cb59170e645
Michael Krog <mic@apaq.dk>
2025-04-20 18:39:46 +0200

    Adds initial stenciljs with angular wrapper

---
+++ packages/stencil-library/stencil.config.ts
@@ -0,0 +1,34 @@
+import { Config } from '@stencil/core';
+import { angularOutputTarget } from '@stencil/angular-output-target';
+
+export const config: Config = {
+  namespace: 'stencil-library',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements',
+      customElementsExportBehavior: 'auto-define-custom-elements',
+      externalRuntime: false,
+    },
+    {
+      type: 'docs-readme',
+    },
+    angularOutputTarget({
+      componentCorePackage: 'stencil-library',
+      outputType: 'standalone',
+      directivesProxyFile: '../angular-workspace/projects/component-library/src/lib/stencil-generated/components.ts',
+      directivesArrayFile: '../angular-workspace/projects/component-library/src/lib/stencil-generated/index.ts',
+    }),
+/*,
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },*/
+  ],
+  testing: {
+    browserHeadless: "shell",
+  },
+};
